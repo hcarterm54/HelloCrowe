2ed4b128f9084ff8ceb42dc43d28763dd84817ce
Carter Mitchell <carter-mitchell@outlook.com>
2020-02-13 23:36:30 -0600

    Add project files.

---
+++ HelloCrowe.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCrowe.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+        }
+    }
+}
